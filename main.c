d595c7fd63a49bf7745274d111919e398064048b
camposdigo <rodrigocordeirocampos@gmail.com>
2025-05-06 17:29:32 -0300

    main.c

--- main.c
+++ main.c
@@ -101,3 +101,35 @@ void substring() {
         printf("A segunda string NÃO está contida na primeira.\n");
     }
 }
+
+//Menu: Rodrigo
+int main() {
+    int opcao;
+
+    printf("===== MENU =====\n");
+    printf("1 - Fibonacci\n");
+    printf("2 - Fatoriais\n");
+    printf("3 - Palindromo\n");
+    printf("4 - Substring\n");
+    printf("Digite sua escolha: ");
+    scanf("%d", &opcao);
+
+    switch (opcao) {
+        case 1:
+            fibonacci();
+            break;
+        case 2:
+            fatoriais();
+            break;
+        case 3:
+            palindromo();
+            break;
+        case 4:
+            substring();
+            break;
+        default:
+            printf("Escolha inválida.\n");
+    }
+
+    return 0;
+}
